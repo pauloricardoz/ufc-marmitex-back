2d3430c25eb5a140ed2e3a6ae8bea13add626a2d
hleoc <informig@gmail.com>
2021-03-21 17:44:48 -0300

    Gerador de token e validação do token

---
+++ Middleware/auth.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+
+const dotenv = require('dotenv');
+
+const User = require('../Models/User');
+
+dotenv.config();
+
+const secret = process.env.SECRET;
+
+module.exports = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+    if (!token) return res.status(401).json({ message: 'missing auth token' });
+    const payload = jwt.verify(token, secret);
+    const user = await User.getByEmail(payload.userData);
+
+    if (!user) {
+      return {
+        error: true,
+        code: 'invalid_data',
+        message: 'invalid token',
+        statusCode: 400,
+      };
+    }
+    req.user = payload.userData;
+
+    return next();
+  } catch (error) {
+    return res.status(401).json({ message: error.message });
+  }
+};
---
+++ Middleware/webTokenMiddleware.js
@@ -0,0 +1,23 @@
+const jwt =  require('jsonwebtoken');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const secret = process.env.SECRET;
+
+const jwtConfig = {
+  expiresIn: '15d',
+  algorithm: 'HS256',
+};
+
+const createToken = (payload) => jwt.sign(payload, secret, jwtConfig);
+
+const verifyToken = async (token) => {
+  const payload = jwt.verify(token, secret);
+  return payload;
+};
+
+module.exports = {
+  createToken,
+  verifyToken,
+};
